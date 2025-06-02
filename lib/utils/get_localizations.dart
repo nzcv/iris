0e3d3e2c03cfac5a093b203e46651c20357ce2e2
22 <60903333+nini22P@users.noreply.github.com>
2025-06-02 17:27:21 +0800

    fix: l10n generate

--- lib/utils/get_localizations.dart
+++ lib/utils/get_localizations.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:iris/l10n/app_localizations.dart';
 
 AppLocalizations getLocalizations(BuildContext context) {
   return AppLocalizations.of(context)!;
